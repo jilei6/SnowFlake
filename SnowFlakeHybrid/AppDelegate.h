1bd2efd95004028abd9b79f27f8360d5b483da26
jaren <jaren@leadadvancetech.com>
2018-06-01 13:58:26 +0800

    Initial Commit

---
+++ SnowFlakeHybrid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnowFlakeHybrid
+//
+//  Created by jilei on 2018/6/1.
+//  Copyright © 2018年 jilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnowFlakeHybrid/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnowFlakeHybrid
+//
+//  Created by jilei on 2018/6/1.
+//  Copyright © 2018年 jilei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
